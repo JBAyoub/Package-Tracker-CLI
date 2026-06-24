8e9ec61edd1d796a6a0277e891b591b7b6b1b068
Ayb <aymojobe@gmail.com>
2026-06-24 12:01:42 +0100

    removed placeholder code

--- bin/package__tracker__c_l_i.dart
+++ bin/package__tracker__c_l_i.dart
@@ -1,5 +1,3 @@
-import 'package:package__tracker__c_l_i/package__tracker__c_l_i.dart' as package__tracker__c_l_i;
-
 void main(List<String> arguments) {
-  print('Hello world: ${package__tracker__c_l_i.calculate()}!');
+  print('Hello world!');
 }
--- test/package__tracker__c_l_i_test.dart
+++ test/package__tracker__c_l_i_test.dart
@@ -1,8 +1,4 @@
-import 'package:package__tracker__c_l_i/package__tracker__c_l_i.dart';
-import 'package:test/test.dart';
+// import 'package:package__tracker__c_l_i/package__tracker__c_l_i.dart';
+// import 'package:test/test.dart';
 
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
+void main() {}
