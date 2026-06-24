7b3dd427c3db543341e862020d4a6b335d0578ba
Ayb <aymojobe@gmail.com>
2026-06-24 11:57:40 +0100

    hello reddit, I have a question

---
+++ bin/package__tracker__c_l_i.dart
@@ -0,0 +1,5 @@
+import 'package:package__tracker__c_l_i/package__tracker__c_l_i.dart' as package__tracker__c_l_i;
+
+void main(List<String> arguments) {
+  print('Hello world: ${package__tracker__c_l_i.calculate()}!');
+}
---
+++ test/package__tracker__c_l_i_test.dart
@@ -0,0 +1,8 @@
+import 'package:package__tracker__c_l_i/package__tracker__c_l_i.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
