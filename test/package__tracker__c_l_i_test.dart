581d613f00f4dcb75e3c0905c8e7bfa1dc5c4a6a
Ayb <aymojobe@gmail.com>
2026-06-29 14:42:11 +0100

    Added Utility validation tests

--- test/package__tracker__c_l_i_test.dart
+++ test/package__tracker__c_l_i_test.dart
@@ -1,4 +1,33 @@
-// import 'package:package__tracker__c_l_i/package__tracker__c_l_i.dart';
-// import 'package:test/test.dart';
+import 'package:package__tracker__c_l_i/utils/validators.dart';
+import 'package:test/test.dart';
 
-void main() {}
+void main() {
+  group("Tracking Number Valid Formats", () {
+    test(" tracking number is empty", () {
+      expect(Validators().validTrackNumber(""), false);
+    });
+    test("tracking number is all spaces", () {
+      expect(Validators().validTrackNumber("   "), false);
+    });
+    test("Tracking number has special chars", () {
+      expect(Validators().validTrackNumber("6546!*+-/==)ç_5645"), false);
+    });
+    test("Tracking number should be valid", () {
+      expect(Validators().validTrackNumber("LA225641500AE"), true);
+    });
+  });
+  group("Correct API Key format", () {
+    test("API Key is empty", () {
+      expect(Validators.isValid(""), false);
+    });
+    test("API Key is all spaces", () {
+      expect(Validators.isValid("                                 "), false);
+    });
+    test("API Key has special chars", () {
+      expect(Validators.isValid("AADSQDQSDSQCSQCQ6546!*+-/==)ç_QSD"), false);
+    });
+    test("API key should be valid", () {
+      expect(Validators.isValid("356A2LK8PO6978S54F58G4E545SS787Z"), true);
+    });
+  });
+}
